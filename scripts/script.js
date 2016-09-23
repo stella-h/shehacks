405f6b0b6904dec19a4f848e83d9a0ee3bf36b2d
Rebecca McCay <rebecca.mccay@mapdataservices.com>
2016-09-24 09:51:53 +1000

    updated goals
    added images folder

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
 
     $(".noShow").hide();
 
+
+
+/*Dashboard*/
+  $("[data-toggle=popover]").popover({
+        html : true, 
+        content: function() {
+          return $("#user-menu").html();
+        }
+    });
+
     $('#goalCheck1').attr('checked', false); 
     $('#goalCheck2').attr('checked', false); 
     $('#goalCheck3').attr('checked', false); 
@@ -9,18 +19,18 @@ $(document).ready(function(){
     $("#goalCheck1").click(function(){
         $(".noGoals").hide();
         $(".goalsSet").show();
-        $("#goal1").show();
+        $("#goal1").toggle();
     });
 
       $("#goalCheck2").click(function(){
         $(".noGoals").hide();
         $(".goalsSet").show();
-        $("#goal2").show();
+        $("#goal2").toggle();
     });
 
       $("#goalCheck3").click(function(){
         $(".noGoals").hide();
         $(".goalsSet").show();
-        $("#goal3").show();
+        $("#goal3").toggle();
     });
 });
